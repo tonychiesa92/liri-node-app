7a478406fc42bb3256abfaa0dfd36f693481616c
Tony C <tonychiesa92@gmail.com>
2019-08-02 01:05:26 -0400

    Added concertThis function to make API call to bands in town

--- liri.js
+++ liri.js
@@ -20,10 +20,63 @@ switch (action) {
         break;
 }
 
+function concertThis() {
+    
+    // Include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
+    var axios = require("axios");
 
+    // Store all of the arguments in an array
+    var nodeArgs = process.argv;
+
+    // Create an empty variable for holding the movie name
+    var artist = "";
+
+    // Loop through all the words in the node argument
+    // And do a little for-loop magic to handle the inclusion of "+"s
+    for (var i = 2; i < nodeArgs.length; i++) {
+
+        if (i > 2 && i < nodeArgs.length) {
+            artist = artist + "+" + nodeArgs[i];
+        } else {
+            artist += nodeArgs[i];
+
+        }
+    }
+
+    // Then run a request with axios to the OMDB API with the movie specified
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    // This line is just to help us debug against the actual URL.
+    console.log(queryUrl);
+
+    axios.get(queryUrl).then(
+        function (response) {
+            console.log("Release Year: " + response.data.Year);
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+}
 
 function movieThis() {
-    
+
     // Include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
     var axios = require("axios");
 
